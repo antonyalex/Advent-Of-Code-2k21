3624b1a7dd48eab3733786dfeabbf5b55b295389
The Awesomeness <58598175+antonyalex@users.noreply.github.com>
2021-12-01 23:24:19 +0530

    Update task1.py

--- Day1/task1.py
+++ Day1/task1.py
@@ -1,1 +1,8 @@
-
+file=open("input.txt","r")
+list=file.readlines()
+c=0
+list2 =[s.strip('\n') for s in list] and [int(s) for s in list]
+for i in range(len(list2)-1):
+    if(list2[i+1]>list2[i]):
+        c+=1
+print (c)
